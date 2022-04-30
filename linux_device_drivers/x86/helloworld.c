940b8f6a13d81501737580d03f1ce63002dcfe51
Nguyen Toan <toannguyenkt69@gmail.com>
2022-04-30 00:56:05 -0700

    clean Delay and Sleep

--- linux_device_drivers/x86/helloworld.c
+++ linux_device_drivers/x86/helloworld.c
@@ -1,22 +1,10 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/delay.h>
 
 static int __init helloworld_init(void)
 {
 	pr_info("Hello World!\n");
-	pr_info("ndelay: 10000ns busy loop\n"); 	
-	ndelay(10000);
-	pr_info("udelay: 10000us busy loop\n");
-	udelay(10000);
-	pr_info("mdelay: 1000ms busy loop\n");
-	mdelay(1000);
-	pr_info("usleep_range: 1000000-2000000us sleep\n");
-	usleep_range(1000000, 2000000);
-	pr_info("msleep: 1000ms sleep\n");
-	msleep(1000);
-	pr_info("Module Init Done!\n");
 	return 0;
 }
 
