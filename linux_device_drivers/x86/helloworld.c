d1d9298b4f8babe7b27cce3af72178b70e63ffea
Nguyen Toan <toannguyenkt69@gmail.com>
2022-04-30 01:13:01 -0700

    Kernel locking mechanism: Mutex

--- linux_device_drivers/x86/helloworld.c
+++ linux_device_drivers/x86/helloworld.c
@@ -1,10 +1,40 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/mutex.h>
+
+#ifdef STATIC_METHOD
+DEFINE_MUTEX(my_mutex);
+#else
+struct mutex my_mutex;
+#endif
 
 static int __init helloworld_init(void)
 {
 	pr_info("Hello World!\n");
+#ifdef STATIC_METHOD
+	
+#else
+	mutex_init(&my_mutex);
+#endif
+
+	// Lock method
+	// void mutex_lock(struct mutex *lock);					//not return if receive signal
+	// int mutex_lock_interruptible(struct mutex *lock); 	//return if receive any signal
+	// int mutex_lock_killable(struct mutex *lock); 		//return if receive kill signal
+
+	// Unlock method
+	// void mutex_unlock(struct mutex *lock);
+
+	// Check lock state
+	// int mutex_is_locked(struct mutex *lock);
+
+	// Lock if available, else return
+	// int mutex_trylock(struct mutex *lock);
+
+	mutex_lock(&my_mutex);
+	mutex_unlock(&my_mutex);
+
 	return 0;
 }
 
