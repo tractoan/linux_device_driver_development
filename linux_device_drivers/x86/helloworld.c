439e67a106eba54ab8276331730ecf721462b2da
Nguyen Toan <toannguyenkt69@gmail.com>
2022-04-30 03:32:20 -0700

    Clean Work deferring mechanism: SoftIRQs and ksoftirqd

--- linux_device_drivers/x86/helloworld.c
+++ linux_device_drivers/x86/helloworld.c
@@ -1,24 +1,6 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include 
-
-/*Software IRQ (softirq), or software interrupt is a deferring mechanism used only for very
-fast processing, since it runs with a disabled scheduler (in an interrupt context). You'll
-rarely (almost never) want to deal with softirq directly. There are only networks and block
-device subsystems using softirq. Tasklets are an instantiation of softirqs, and will be
-sufficient in almost every case that you feel the need to use softirqs*/
-
-/*In most cases, softirqs are scheduled in hardware interrupts, which may arrive very quickly,
-faster than they can be serviced. They are then queued by the kernel in order to be
-processed later. Ksoftirqds are responsible for late execution (process context this time). A
-ksoftirqd is a per-CPU kernel thread raised to handle unserviced software interrupts*/
-
-/*When run "top", you can see ksoftirqd/n 
-entries, where n is the CPU number that the ksoftirqd runs on. CPU-consuming ksoftirqd
-may indicate an overloaded system or a system under interrupts storm, which is never
-good. You can have a look at kernel/softirq.c to see how ksoftirqds are designed.
-*/
 
 static int __init helloworld_init(void)
 {
