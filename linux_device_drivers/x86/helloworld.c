de6a8cf78ce12ec68bdc0d20cfb8d2df515b468d
Nguyen Toan <toannguyenkt69@gmail.com>
2022-04-30 03:17:33 -0700

    Kernel locking mechanism: Spinlock

--- linux_device_drivers/x86/helloworld.c
+++ linux_device_drivers/x86/helloworld.c
@@ -1,10 +1,30 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/spinlock.h>
+
+spinlock_t my_spinlock;
 
 static int __init helloworld_init(void)
 {
 	pr_info("Hello World!\n");
+	// Init
+	// spin_lock_init(spinlock_t *lock);
+
+	// Lock
+	// void spin_lock(spinlock_t *lock);
+	// int spin_trylock(spinlock_t *lock);
+	// void spin_unlock(spinlock_t *lock);
+
+	// Lock and Block Bottom Half
+	// void spin_lock_bh(spinlock_t *lock);
+	// int spin_trylock_bh(spinlock_t *lock)
+	// void spin_unlock_bh(spinlock_t *lock);
+
+	// Lock and Block IRQ
+	// void spin_lock_irqsave(spinlock_t *lock);
+	// int spin_trylock_irqsave(spinlock_t *lock);
+	// void spin_unlock_irqrestore(spinlock_t *lock, unsigned long flags)
 	return 0;
 }
 
