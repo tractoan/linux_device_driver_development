1af99d8f2f293edc230f2ede54f19d48edcb5297
Nguyen Toan <toannguyenkt69@gmail.com>
2022-05-11 07:55:38 -0700

    Invoking user-space applications from the kernel

--- linux_device_drivers/x86/helloworld.c
+++ linux_device_drivers/x86/helloworld.c
@@ -1,16 +1,39 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/workqueue.h> /* for work queue */
+#include <linux/kmod.h>
+
+static struct delayed_work shutdown_work;
+static void delayed_shutdown(struct work_struct *work)
+{
+	char *cmd = "/sbin/shutdown";
+	char *argv[] = {
+						cmd,
+						"-h",
+						"now",
+						NULL,
+					};
+	char *envp[] = {
+						"HOME=/",
+						"PATH=/sbin:/bin:/usr/sbin:/usr/bin",
+						NULL,
+					};
+	call_usermodehelper(cmd, argv, envp, 0);
+}
 
 static int __init helloworld_init(void)
 {
 	pr_info("Hello World!\n");
+	INIT_DELAYED_WORK(&shutdown_work, delayed_shutdown);
+	schedule_delayed_work(&shutdown_work, msecs_to_jiffies(800));
 	return 0;
 }
 
 static void __exit helloworld_exit(void)
 {
 	pr_info("End of the world!\n");
+	cancel_delayed_work_sync(&shutdown_work);
 }
 
 module_init(helloworld_init);
