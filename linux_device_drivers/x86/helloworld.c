1726f393482b12e13d91cb5db2609b2e6de399c2
Nguyen Toan <toannguyenkt69@gmail.com>
2022-05-12 06:53:23 -0700

    Character Device Drivers

--- linux_device_drivers/x86/helloworld.c
+++ linux_device_drivers/x86/helloworld.c
@@ -1,40 +1,34 @@
+/*
+	Character devices transfer data to or from a user application by means of characters, in a
+stream manner (one character after another), like a serial port does. A character device
+driver exposes the properties and functionalities of a device by means of a special file in the
+/dev directory, which one can use to exchange data between the device and user
+application, and also allows you to control the real physical device. This is the basic concept
+of Linux that says everything is a file. A character device driver represents the most basic
+device driver in the kernel source. Character devices are represented in the kernel as
+instances of struct cdev, defined in include/linux/cdev.h:
+	struct cdev {
+		struct kobject kobj;
+		struct module *owner;
+		const struct file_operations *ops;
+		struct list_head list;
+		dev_t dev;
+		unsigned int count;
+	};
+*/
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/workqueue.h> /* for work queue */
-#include <linux/kmod.h>
-
-static struct delayed_work shutdown_work;
-static void delayed_shutdown(struct work_struct *work)
-{
-	pr_info("Preparing Shutdown ... \n");
-	char *cmd = "/sbin/shutdown";
-	char *argv[] = {
-						cmd,
-						"-h",
-						"now",
-						NULL,
-					};
-	char *envp[] = {
-						"HOME=/",
-						"PATH=/sbin:/bin:/usr/sbin:/usr/bin",
-						NULL,
-					};
-	call_usermodehelper(cmd, argv, envp, 0);
-}
 
 static int __init helloworld_init(void)
 {
 	pr_info("Hello World!\n");
-	INIT_DELAYED_WORK(&shutdown_work, delayed_shutdown);
-	schedule_delayed_work(&shutdown_work, msecs_to_jiffies(800));
 	return 0;
 }
 
 static void __exit helloworld_exit(void)
 {
 	pr_info("End of the world!\n");
-	cancel_delayed_work_sync(&shutdown_work);
 }
 
 module_init(helloworld_init);
