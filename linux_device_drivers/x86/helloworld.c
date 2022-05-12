303b6403e32aab684edde272b202ea95a5c776bf
Nguyen Toan <toannguyenkt69@gmail.com>
2022-05-12 07:54:25 -0700

    Allocating and registering a character device

--- linux_device_drivers/x86/helloworld.c
+++ linux_device_drivers/x86/helloworld.c
@@ -1,16 +1,127 @@
+/*	
+	Character devices are represented in the kernel as instances of struct cdev. When writing
+a character device driver, your goal is to finally create and register an instance of that
+structure associated with a struct file_operations, exposing a set of operations
+(functions) the user-space can perform on the device. To reach that goal, there are some
+steps we must go through, which are as follows:
+	1. Reserve a major and a range of minors with alloc_chrdev_region().
+	2. Create a class for your devices with class_create(), visible in /sys/class/.
+	3. Set up a struct file_operation (to be given to cdev_init), and for each3.
+	device one needs to create, call cdev_init() and cdev_add() to register the
+	device.
+	4. Then create a device_create() for each device, with a proper name. It will4.
+	result in your device being created in the /dev directory
+*/
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+
+dev_t dev = 0;
+static struct class *dev_class;
+static struct cdev etx_cdev;
+
+/*
+** Function Prototypes
+*/
+static int      etx_open(struct inode *inode, struct file *file);
+static int      etx_release(struct inode *inode, struct file *file);
+static ssize_t  etx_read(struct file *filp, char __user *buf, size_t len,loff_t * off);
+static ssize_t  etx_write(struct file *filp, const char *buf, size_t len, loff_t * off);
+
+static struct file_operations fops =
+{
+    .owner      = THIS_MODULE,
+    .read       = etx_read,
+    .write      = etx_write,
+    .open       = etx_open,
+    .release    = etx_release,
+};
+
+/*
+** This function will be called when we open the Device file
+*/
+static int etx_open(struct inode *inode, struct file *file)
+{
+        pr_info("Driver Open Function Called...!!!\n");
+        return 0;
+}
+
+/*
+** This function will be called when we close the Device file
+*/
+static int etx_release(struct inode *inode, struct file *file)
+{
+        pr_info("Driver Release Function Called...!!!\n");
+        return 0;
+}
+
+/*
+** This function will be called when we read the Device file
+*/
+static ssize_t etx_read(struct file *filp, char __user *buf, size_t len, loff_t *off)
+{
+        pr_info("Driver Read Function Called...!!!\n");
+        return 0;
+}
+
+/*
+** This function will be called when we write the Device file
+*/
+static ssize_t etx_write(struct file *filp, const char __user *buf, size_t len, loff_t *off)
+{
+        pr_info("Driver Write Function Called...!!!\n");
+        return len;
+}
 
 static int __init helloworld_init(void)
 {
-	pr_info("Hello World!\n");
+	/*Allocating Major number*/
+	if((alloc_chrdev_region(&dev, 0, 1, "etx_Dev")) <0){
+			pr_err("Cannot allocate major number\n");
+			return -1;
+	}
+	pr_info("Major = %d Minor = %d \n",MAJOR(dev), MINOR(dev));
+
+	/*Creating cdev structure*/
+	cdev_init(&etx_cdev,&fops);
+
+	/*Adding character device to the system*/
+	if((cdev_add(&etx_cdev,dev,1)) < 0){
+		pr_err("Cannot add the device to the system\n");
+		goto r_class;
+	}
+
+	/*Creating struct class*/
+	if((dev_class = class_create(THIS_MODULE,"etx_class")) == NULL){
+		pr_err("Cannot create the struct class\n");
+		goto r_class;
+	}
+
+	/*Creating device*/
+	if((device_create(dev_class,NULL,dev,NULL,"etx_device")) == NULL){
+		pr_err("Cannot create the Device 1\n");
+		goto r_device;
+	}
+	pr_info("Device Driver Insert...Done!!!\n");
 	return 0;
+
+r_device:
+	class_destroy(dev_class);
+r_class:
+	unregister_chrdev_region(dev,1);
+	return -1;
 }
 
 static void __exit helloworld_exit(void)
 {
-	pr_info("End of the world!\n");
+	device_destroy(dev_class,dev);
+        class_destroy(dev_class);
+        cdev_del(&etx_cdev);
+        unregister_chrdev_region(dev, 1);
+        pr_info("Device Driver Remove...Done!!!\n");
 }
 
 module_init(helloworld_init);
